71ddca4bb247be94cc835a24f0afd1f5ed5337a1
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-06-11 13:49:31 +0600

    first unit test added

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,7 +1,15 @@
-const Todocontroller = require("../../controller/todo.controller");
+const TodoController = require("../../controller/todo.controller");
+const TodoModel = require("../../model/todo.model");
 
-describe("Todocontroller.createTodo", () => {
+// mock the create function
+TodoModel.create = jest.fn();
+
+describe("TodoController.createTodo", () => {
   it("should have a createTodo function", () => {
-    expect(typeof Todocontroller.createTodo).toBe("function");
+    expect(typeof TodoController.createTodo).toBe("function");
+  });
+  it("should call Todomodel.create", () => {
+    TodoController.createTodo();
+    expect(TodoModel.create).toBeCalled();
   });
 });
