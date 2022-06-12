c3f60646fe6faf4b2fc5cb309a4a2c8d2c3131af
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-06-12 21:19:31 +0600

    response test case added and resolved

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -1,6 +1,6 @@
 const TodoModel = require("../model/todo.model");
 
 exports.createTodo = (req, res, next) => {
-  console.log("req data", req.body);
   TodoModel.create(req.body);
+  res.status(201);
 };
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -22,4 +22,9 @@ describe("TodoController.createTodo", () => {
     TodoController.createTodo(req, res, next);
     expect(TodoModel.create).toBeCalledWith(newTodo);
   });
+  it("should return 201 response code", () => {
+    req.body = newTodo;
+    TodoController.createTodo(req, res, next);
+    expect(res.statusCode).toBe(201);
+  });
 });
